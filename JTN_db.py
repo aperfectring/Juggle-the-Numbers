d23fe589de3cfdce44a10cb6b46d067a164261a6
aperfectring <aperfectring@APRnewuser.ftrdhcpuser.net>
2014-02-05 16:11:57 -0800

    Finished migration of Results Notebook to JTN_db and away from referencing parent.

--- JTN_db.py
+++ JTN_db.py
@@ -342,11 +342,15 @@ class JTN_db:
 		self.commit()
 
 	# 0:season_id, 1:date, 2:home_id, 3:home_goals, 4:home_pks, 5:away_id, 6:away_goals, 7:away_pks, 8:aet, 9:pks, 10:played, 11:game_style, 12:game_id, 13:attendance
-	def get_all_games(self, season_id = None, ordered = False, played = None, start_date = None, end_date = None, date = None, home_team = None, away_team = None, any_team = None, home_win = None, away_win = None, game_tied = None, unplayed_after = None):
+	def get_all_games(self, season_id = None, ordered = False, played = None, start_date = None, end_date = None, date = None, home_team = None, away_team = None, any_team = None, home_win = None, away_win = None, game_tied = None, unplayed_after = None, order_asc = None):
 		order_text = ""
 		where_parts = []
 		if ordered == True:
-			order_text = " ORDER BY date DESC"
+			order_text = " ORDER BY date"
+			if order_asc != None:
+				order_text += " ASC"
+			else:
+				order_text += " DESC"
 
 		if season_id != None:
 			where_parts.append("season_id='" + str(season_id) + "'")
--- Results_Notebook.py
+++ Results_Notebook.py
@@ -3,13 +3,12 @@ import gtk
 import gobject
 
 class Results_Notebook:
-	def __init__(self, parent, parent_box, JTN_db):
-		self.parent = parent
+	def __init__(self, parent_box, get_season_id, JTN_db):
 		self.parent_box = parent_box
+		self.get_season_id = get_season_id
 		self.JTN_db = JTN_db
 		
 
-		season_id = self.parent.season_combo.get_id()
 		#### There are enough strings here for 52 games in the season, AND their background colors.
 		####    This is much more than we need, but better to be safe than sorry.
 		list_store = gtk.ListStore(gobject.TYPE_STRING,
@@ -136,7 +135,7 @@ class Results_Notebook:
 
 	### Repopulate the results table from the DB
 	def repop(self):
-		season_id = self.parent.season_combo.get_id()
+		season_id = self.get_season_id()
 		
 		all_list = self.all_view.get_model()
 		all_list.clear()
@@ -148,19 +147,16 @@ class Results_Notebook:
 		self.all_view.append_column(column)
 
 		### Add columns to the treeview for each game in the season for each team.
-		self.parent.cur.execute("SELECT team_id FROM team_season WHERE season_id = '" + str(season_id) + "'")
-		row = self.parent.cur.fetchone()
-		if row:
-			team_id = row[0]
+		row = self.JTN_db.get_teams(season_id = season_id)
+		if len(row):
+			team_id = row[0][0]
 		else:
 			team_id = 0
 
-		self.parent.cur.execute("SELECT COUNT(*) FROM games WHERE (season_id = '" + str(season_id) + "' AND (home_id = '" + str(team_id) + "' OR away_id = '" + str(team_id) + "'))")
-		row = self.parent.cur.fetchone()
-		if row:
-			for n in range(0,row[0]):
-				column = gtk.TreeViewColumn(str(n+1), gtk.CellRendererText(), text=(n*2+1), background=(n*2+2))
-				self.all_view.append_column(column)
+		count = len(self.JTN_db.get_all_games(season_id = season_id, any_team = team_id))
+		for n in range(0,count):
+			column = gtk.TreeViewColumn(str(n+1), gtk.CellRendererText(), text=(n*2+1), background=(n*2+2))
+			self.all_view.append_column(column)
 
 		team_list = []
 		### For each team in the league...
@@ -171,39 +167,37 @@ class Results_Notebook:
 			team_row = [name]
 
 			### Fetch the results for each game for the current team (sorted by date)
-			self.parent.cur.execute("SELECT home_id, home_goals, away_id, away_goals, played FROM games WHERE (season_id = '" + str(season_id) + "' AND (home_id = '" + str(team_id) + "' OR away_id = '" + str(team_id) + "')) ORDER BY date")
-
-			for game in self.parent.cur.fetchall():
-				other_team_id = game[0]
-				if game[0] == team_id:
-					other_team_id = game[2]
-					other_goals = game[3]
-					goals = game[1]
+			for game in self.JTN_db.get_all_games(season_id = season_id, ordered = True, any_team = team_id, order_asc = True):
+				if game[2] == team_id:
+					other_team_id = game[5]
+					other_goals = game[6]
+					goals = game[3]
 					prefix = ""
 				else:
-					other_team_id = game[0]
-					other_goals = game[1]
-					goals = game[3]
+					other_team_id = game[2]
+					other_goals = game[3]
+					goals = game[6]
 					prefix = "@"
-				
+		
 				(other_team_id, other_team_name, other_team_city, other_team_abbr) = self.JTN_db.get_team(team_id = other_team_id)
 				if other_team_abbr != None:
 					other_team_abbr = prefix + other_team_abbr
 				else:
 					other_team_abbr = prefix
 				text = other_team_abbr + "\n"
-				if game[4] == "TRUE":
-					text += str(game[1])
+
+				if game[10] == "TRUE":
+					text += str(game[3])
 				else:
 					text += " "
 				text += "-"
-				if game[4] == "TRUE":
-					text += str(game[3])
+				if game[10] == "TRUE":
+					text += str(game[6])
 				else:
 					text += " "
 				team_row.append(text)
 
-				if game[4] == "FALSE":
+				if game[10] == "FALSE":
 					bg = "#FFFFFF"
 				elif goals > other_goals:
 					bg = "#80FF80"
@@ -212,6 +206,7 @@ class Results_Notebook:
 				else:
 					bg = "#FFFF80"
 				team_row.append(bg)
+
 
 			### Add the results for all of the games into the treeview
 			for n in range(len(team_row),all_list.get_n_columns()):
